2af3f261508f0976e94638f2f3b7185ffa4ece46
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-04-06 09:53:12 +0530

    add progress bar to Mega-to-GDrive

--- src/components/Transfers/TransfersBase/TransferringComponent.js
+++ src/components/Transfers/TransfersBase/TransferringComponent.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
 
 /**
  *
  * @param {string} primaryText
+ * @param {string} secondaryText
+ * @param {number} percentage
  * @returns {JSX.Element}
  * @constructor
  */
-const TransferringComponent = ({ primaryText }) => (
+const TransferringComponent = ({ primaryText, secondaryText, percentage }) => (
   <React.Fragment>
     <Typography
       sx={{ display: "inline" }}
@@ -18,6 +22,32 @@ const TransferringComponent = ({ primaryText }) => (
       {primaryText}
     </Typography>
     <br />
+    {secondaryText && (
+      <>
+        <Typography
+          sx={{ display: "inline" }}
+          component="span"
+          variant="body2"
+          color="text.secondary"
+        >
+          {secondaryText}
+        </Typography>
+        <br />
+      </>
+    )}
+    {percentage && (
+      <Box sx={{ width: "100%" }} component="span">
+        <LinearProgress
+          variant="buffer"
+          value={percentage}
+          valueBuffer={percentage}
+          color="inherit"
+          sx={{
+            marginTop: "10px",
+          }}
+        />
+      </Box>
+    )}
   </React.Fragment>
 );
 
