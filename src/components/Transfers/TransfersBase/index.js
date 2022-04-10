c383f7cf63d592df4438f4a581d02c591627e5af
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-04-11 05:05:41 +0530

    refactor transferring component

--- src/components/Transfers/TransfersBase/index.js
+++ src/components/Transfers/TransfersBase/index.js
@@ -5,9 +5,13 @@ import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { useDispatch, useSelector } from "react-redux";
-import { clearMessages } from "../../../store/actions";
+import { clearMessages, checkAPIAlive } from "../../../store/actions";
+import Box from "@mui/material/Box";
+import FormGroup from "@mui/material/FormGroup";
+import TextField from "@mui/material/TextField";
 import Message from "../../../helpers/Notification";
-import { useFirebaseConnect } from "react-redux-firebase";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { useFirebase, useFirebaseConnect } from "react-redux-firebase";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
@@ -15,49 +19,19 @@ import ListItemText from "@mui/material/ListItemText";
 import FolderIcon from "@mui/icons-material/Folder";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
+import Tooltip from "@mui/material/Tooltip";
 import Skeleton from "@mui/material/Skeleton";
+import CircularProgress from "@mui/material/CircularProgress";
 import { get } from "lodash";
+import InputAdornment from "@mui/material/InputAdornment";
+import SyncProblemIcon from "@mui/icons-material/SyncProblem";
+import SyncIcon from "@mui/icons-material/Sync";
+import { red } from "@mui/material/colors";
 import useFCMNotifications from "../../../helpers/useFCMNotifications";
 import useEnableFCM from "../../../helpers/useEnableFCM";
 import CustomizedToolTip from "../../../helpers/CustomizedToolTip";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons/faSync";
-import InputComponent from "./InputComponent";
-
-const classes = makeStyles((theme) => ({
-  root: {
-    minWidth: 350,
-  },
-  glass: {
-    background: "rgba(255, 255, 255, 0.1)",
-    borderRadius: "1px",
-    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-    backdropFilter: "blur(2.1px)",
-    border: "1px solid rgba(255, 255, 255, 0.24)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  large: {
-    paddingBottom: "6px",
-    marginBottom: "2px",
-    filter: "drop-shadow(0px 0px 15px #222)",
-  },
-  typography: {
-    flexGrow: 1,
-    textAlign: "center",
-    color: theme.palette.text.primary,
-  },
-  button: {
-    marginTop: theme.spacing(2),
-    fontSize: "18px",
-    width: "150px",
-    "&:hover": {
-      border: `1px solid ${theme.palette.action.success}`,
-      color: theme.palette.action.success,
-    },
-  },
-}))();
 
 /**
  *
@@ -82,14 +56,17 @@ const TransfersBase = ({
   secondaryComponent,
   transferringComponent,
 }) => {
+  const firebase = useFirebase();
   const dispatch = useDispatch();
-
+  const [link, setLink] = useState("");
+  const [errorOpen, setErrorOpen] = useState(false);
+  const [successOpen, setSuccessOpen] = useState(false);
   const [notificationOpen, setNotificationOpen] = useState(false);
-
+  const [validationError, setValidationError] = useState("");
   const [transfers, setTransfers] = useState([]);
   const [transferring, setTransferring] = useState([]);
   const [notification, setNotification] = useState(null);
-
+  const [apiAlive, setApiAlive] = useState(null);
   const [transfersLoading, setTransfersLoading] = useState(null);
   const [transferringLoading, setTransferringLoading] = useState(null);
 
@@ -131,12 +108,114 @@ const TransfersBase = ({
     setTransferring((orderedData || []).reverse().map((obj) => obj.value));
   }, [dbPath, uid, ordered]);
 
+  const error = useSelector(
+    ({
+      userData: {
+        transfer: { error },
+      },
+    }) => error
+  );
+  const loading = useSelector(
+    ({
+      userData: {
+        transfer: { loading },
+      },
+    }) => loading
+  );
+
+  const onErrorNotificationClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setErrorOpen(false);
+  };
+
+  const onSuccessNotificationClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    clearMessages()(dispatch);
+    setSuccessOpen(false);
+  };
+
   const onNotificationClose = (event, reason) => {
     if (reason === "clickaway") {
       return;
     }
     setNotificationOpen(false);
   };
+
+  useEffect(() => {
+    if (error) {
+      setErrorOpen(true);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (error === false && loading === false) {
+      setSuccessOpen(true);
+    }
+  }, [error, loading]);
+
+  const classes = makeStyles((theme) => ({
+    root: {
+      minWidth: 350,
+    },
+    glass: {
+      background: "rgba(255, 255, 255, 0.1)",
+      borderRadius: "1px",
+      boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+      backdropFilter: "blur(2.1px)",
+      border: "1px solid rgba(255, 255, 255, 0.24)",
+    },
+    title: {
+      fontSize: 14,
+    },
+    large: {
+      paddingBottom: "6px",
+      marginBottom: "2px",
+      filter: "drop-shadow(0px 0px 15px #222)",
+    },
+    typography: {
+      flexGrow: 1,
+      textAlign: "center",
+      color: theme.palette.text.primary,
+    },
+    button: {
+      marginTop: theme.spacing(2),
+      fontSize: "18px",
+      width: "150px",
+      "&:hover": {
+        border: `1px solid ${theme.palette.action.success}`,
+        color: theme.palette.action.success,
+      },
+    },
+  }))();
+
+  const onSubmit = () => {
+    const regExp = new RegExp(regExpString);
+
+    if (!regExp.test(link)) {
+      setValidationError(validationErrorMessage);
+      return;
+    }
+    submitFN(link, dbPath)(firebase, dispatch);
+    setValidationError("");
+    setLink("");
+    clearMessages()(dispatch);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkAPIAlive()(firebase)
+        .then(() => setApiAlive(true))
+        .catch(() => setApiAlive(false));
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [firebase]);
 
   const requesting = useSelector(({ firebase: { requesting } }) => requesting);
   const requested = useSelector(({ firebase: { requested } }) => requested);
@@ -171,8 +250,47 @@ const TransfersBase = ({
     }
   }, [requesting, requested, uid, dbPath]);
 
+  const copyFromClipboard = async () => {
+    if (!("clipboard" in navigator)) return;
+    const permissions = await navigator.permissions.query({
+      name: "clipboard-read",
+    });
+    if (permissions.state !== "granted" && permissions.state !== "prompt")
+      return;
+    const clipText = await navigator.clipboard.readText();
+    const regExp = new RegExp(regExpString);
+    if (!regExp.test(clipText)) return;
+    setValidationError("");
+    setLink(clipText);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      onSubmit();
+    }
+  };
+
   return (
     <div className="container">
+      {error && (
+        <Message
+          severity={"error"}
+          onClose={onErrorNotificationClose}
+          message={error}
+          open={errorOpen}
+          autoHideDuration={2000}
+        />
+      )}
+      {successOpen && (
+        <Message
+          severity={"success"}
+          onClose={onSuccessNotificationClose}
+          message={"Transfer Queued!"}
+          open={successOpen}
+          autoHideDuration={2000}
+        />
+      )}
       {notification && (
         <Message
           severity={"info"}
@@ -183,16 +301,80 @@ const TransfersBase = ({
           autoHideDuration={5000}
         />
       )}
-
-      <InputComponent
-        dbPath={dbPath}
-        classes={classes}
-        regExpString={regExpString}
-        validationErrorMessage={validationErrorMessage}
-        submitFN={submitFN}
-        title={title}
-        placeholder={placeholder}
-      />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "30vh" }}
+      >
+        <Grid item sx={{ textAlign: "center", minWidth: "50vw" }}>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography
+                variant="h5"
+                className={classes.typography}
+                sx={{ marginBottom: "10px" }}
+              >
+                {title}
+              </Typography>
+              <Box component="form" noValidate autoComplete="off">
+                <FormGroup>
+                  <TextField
+                    disabled={!apiAlive}
+                    sx={{ mb: 1 }}
+                    placeholder={placeholder}
+                    value={link}
+                    onChange={(event) => {
+                      setValidationError("");
+                      setLink(event.target.value);
+                    }}
+                    onFocus={copyFromClipboard}
+                    onKeyDown={handleKeyDown}
+                    required
+                    error={!!validationError}
+                    label={validationError ? "Error" : ""}
+                    helperText={!!validationError ? validationError : ""}
+                    variant="standard"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          {apiAlive === null ? (
+                            <Tooltip title="Checking Niwder-API status">
+                              <CircularProgress color="inherit" size="1.2rem" />
+                            </Tooltip>
+                          ) : apiAlive === true ? (
+                            <Tooltip title="Niwder-API is active">
+                              <SyncIcon color="success" />
+                            </Tooltip>
+                          ) : (
+                            <Tooltip title="Niwder-API is unreachable">
+                              <SyncProblemIcon sx={{ color: red[500] }} />
+                            </Tooltip>
+                          )}
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </FormGroup>
+              </Box>
+              <LoadingButton
+                type="submit"
+                variant="outlined"
+                color="inherit"
+                size={"large"}
+                className={classes.button}
+                disabled={!link}
+                onClick={onSubmit}
+                loading={loading}
+              >
+                Start Transfer
+              </LoadingButton>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
 
       <Grid
         container
