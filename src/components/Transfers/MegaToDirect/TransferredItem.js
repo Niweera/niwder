5f4ebe0875086077d6cd374c48e011c63906c203
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-05-06 13:24:14 +0530

    refactor mega to direct components fix rendering issue in transferred component

---
+++ src/components/Transfers/MegaToDirect/TransferredItem.js
@@ -0,0 +1,146 @@
+import React, { useCallback, useState } from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import FolderIcon from "@mui/icons-material/Folder";
+import ListItemText from "@mui/material/ListItemText";
+import CustomizedToolTip from "../../../helpers/CustomizedToolTip";
+import Typography from "@mui/material/Typography";
+import ConfirmationDialog from "../../../helpers/ConfirmationDialog";
+import { confirmationMessages } from "../../../config/Constants";
+import { removeTransferred } from "../../../store/actions";
+import { common, red } from "@mui/material/colors";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import Link from "@mui/material/Link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import prettyBytes from "pretty-bytes";
+import TimeAgo from "react-time-ago";
+import { useFirebase } from "react-redux-firebase";
+import { faLink } from "@fortawesome/free-solid-svg-icons/faLink";
+import { faM } from "@fortawesome/free-solid-svg-icons/faM";
+import { get } from "lodash";
+
+const TransferredItem = ({ classes, data, dbPath, id }) => {
+  const { key, name, directLink, megaLink, size, mimeType, timestamp } = data;
+
+  const [open, setOpen] = useState(false);
+  const firebase = useFirebase();
+
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const handleRemoveTransferred = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const onRemoveTransferred = useCallback(
+    () => removeTransferred(dbPath, key)(firebase),
+    [firebase, dbPath, key]
+  );
+
+  return (
+    <ListItem alignItems="flex-start" className={classes.glass} key={id}>
+      <ListItemAvatar>
+        <Avatar>
+          <FolderIcon color="action" />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        primary={
+          <CustomizedToolTip arrow placement="top" title={name}>
+            <Typography
+              sx={{
+                display: "inline-block",
+                maxWidth: "40vw",
+              }}
+              component="span"
+              variant="h6"
+              color="text.primary"
+              noWrap
+            >
+              {name}
+            </Typography>
+          </CustomizedToolTip>
+        }
+        secondary={
+          <React.Fragment>
+            <Link
+              href={directLink}
+              underline={"hover"}
+              variant="body1"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="white"
+            >
+              <FontAwesomeIcon icon={faLink} /> Direct Link
+            </Link>
+            <br />
+            <Link
+              href={megaLink}
+              underline={"hover"}
+              variant="body1"
+              target="_blank"
+              color="white"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faM} /> Mega.nz Link
+            </Link>
+            <br />
+            <Typography
+              sx={{ display: "inline" }}
+              component="span"
+              variant="body2"
+              color="text.primary"
+            >
+              {prettyBytes(size)} [{mimeType}]
+            </Typography>
+            <br />
+            <Typography
+              sx={{ display: "inline" }}
+              component="span"
+              variant="body2"
+              color="text.primary"
+            >
+              Transferred: <TimeAgo date={timestamp} />
+            </Typography>
+          </React.Fragment>
+        }
+      />
+      <ListItemAvatar
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        <ConfirmationDialog
+          id="remove-completed"
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          primaryMessage={"Removing Transferred File"}
+          secondaryMessage={confirmationMessages[dbPath]}
+          action={onRemoveTransferred}
+        />
+        <CustomizedToolTip arrow placement="top" title="Remove">
+          <Avatar
+            sx={{
+              width: 24,
+              height: 24,
+              bgcolor: common["black"],
+              cursor: "pointer",
+            }}
+            onClick={handleRemoveTransferred}
+          >
+            <RemoveCircleIcon sx={{ color: red["A700"] }} fontSize="inherit" />
+          </Avatar>
+        </CustomizedToolTip>
+      </ListItemAvatar>
+    </ListItem>
+  );
+};
+
+const areEqual = (prevProps, nextProps) =>
+  get(prevProps, "id", "") === get(nextProps, "id", "");
+
+export default React.memo(TransferredItem, areEqual);
--- src/components/Transfers/MegaToDirect/index.js
+++ src/components/Transfers/MegaToDirect/index.js
@@ -1,44 +1,104 @@
-import React from "react";
-import { queueMegaTransfer } from "../../../store/actions";
-import TransfersBase from "../TransfersBase";
-import SecondaryComponent from "../TransfersBase/SecondaryComponent";
-import TransferringComponent from "../TransfersBase/TransferringComponent";
-import { faLink } from "@fortawesome/free-solid-svg-icons/faLink";
+import React, { useEffect, useState } from "react";
+import { clearMessages } from "../../../store/actions";
 import { MEGA_TO_DIRECT_QUEUE, megaRe } from "../../../config/Constants";
-import { faM } from "@fortawesome/free-solid-svg-icons/faM";
-
-const secondary = ({ megaLink, directLink, size, mimeType, timestamp }) => (
-  <SecondaryComponent
-    primaryLink={directLink}
-    primaryText={"Direct Link"}
-    secondaryLink={megaLink}
-    secondaryText={"Mega.nz Link"}
-    size={size}
-    mimeType={mimeType}
-    timestamp={timestamp}
-    primaryIcon={faLink}
-    secondaryIcon={faM}
-  />
-);
-
-const transferring = ({ message, percentage }) => (
-  <TransferringComponent primaryText={message} percentage={percentage} />
-);
+import Message from "../../../helpers/Notification";
+import InputComponent from "./InputComponent";
+import TransferringComponent from "./TransferringComponent";
+import TransferredComponent from "./TransferredComponent";
+import { useDispatch } from "react-redux";
+import useEnableFCM from "../../../helpers/useEnableFCM";
+import useFCMNotifications from "../../../helpers/useFCMNotifications";
+import { makeStyles } from "@mui/styles";
 
 const MegaToDirect = () => {
+  const dispatch = useDispatch();
+  const [notificationOpen, setNotificationOpen] = useState(false);
+  const [notification, setNotification] = useState(null);
+
+  useEnableFCM();
+  useFCMNotifications(setNotification, setNotificationOpen);
+
+  useEffect(() => () => clearMessages()(dispatch), [dispatch]);
+
+  const onNotificationClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setNotificationOpen(false);
+  };
+
+  const classes = makeStyles((theme) => ({
+    root: {
+      minWidth: 350,
+    },
+    glass: {
+      background: "rgba(255, 255, 255, 0.1)",
+      borderRadius: "1px",
+      boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+      backdropFilter: "blur(2.1px)",
+      border: "1px solid rgba(255, 255, 255, 0.24)",
+      alignItems: "flex-start",
+    },
+    title: {
+      fontSize: 14,
+    },
+    large: {
+      paddingBottom: "6px",
+      marginBottom: "2px",
+      filter: "drop-shadow(0px 0px 15px #222)",
+    },
+    typography: {
+      flexGrow: 1,
+      textAlign: "center",
+      color: theme.palette.text.primary,
+    },
+    button: {
+      marginTop: theme.spacing(2),
+      fontSize: "18px",
+      width: "150px",
+      "&:hover": {
+        border: `1px solid ${theme.palette.action.success}`,
+        color: theme.palette.action.success,
+      },
+    },
+  }))();
+
   return (
-    <TransfersBase
-      dbPath={MEGA_TO_DIRECT_QUEUE}
-      regExpString={megaRe}
-      validationErrorMessage={
-        "The URL must be a valid Mega.nz file/folder export URL"
-      }
-      submitFN={queueMegaTransfer}
-      title={<>Add a Mega.nz link to convert to a Direct link</>}
-      placeholder={"Mega.nz Link"}
-      secondaryComponent={secondary}
-      transferringComponent={transferring}
-    />
+    <div className="container">
+      {notification?.title && (
+        <Message
+          severity={"info"}
+          alertTitle={`Transfer completed: ${notification.title}`}
+          onClose={onNotificationClose}
+          message={notification.body}
+          open={notificationOpen}
+          autoHideDuration={5000}
+        />
+      )}
+
+      {notification?.job && (
+        <Message
+          severity={"error"}
+          alertTitle={"Error occurred in transfer"}
+          onClose={onNotificationClose}
+          message={
+            <>
+              Error [{notification.error}] <br />
+              occurred in transferring <br />
+              {notification.job} <br />
+            </>
+          }
+          open={notificationOpen}
+          autoHideDuration={20000}
+        />
+      )}
+
+      <InputComponent classes={classes} regExpString={megaRe} />
+
+      <TransferringComponent classes={classes} dbPath={MEGA_TO_DIRECT_QUEUE} />
+
+      <TransferredComponent classes={classes} dbPath={MEGA_TO_DIRECT_QUEUE} />
+    </div>
   );
 };
 
