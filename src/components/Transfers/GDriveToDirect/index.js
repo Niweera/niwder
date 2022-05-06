22588123662c18a8939bd565407cc317121f003f
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-05-06 23:57:11 +0530

    refactor google drive to direct components (update) fix rendering issue in transferred component

--- src/components/Transfers/GDriveToDirect/index.js
+++ src/components/Transfers/GDriveToDirect/index.js
@@ -1,43 +1,27 @@
 import React from "react";
 import { queueTransfer } from "../../../store/actions";
-import TransfersBase from "../TransfersBase";
-import SecondaryComponent from "../TransfersBase/SecondaryComponent";
-import TransferringComponent from "../TransfersBase/TransferringComponent";
 import { faGoogleDrive } from "@fortawesome/free-brands-svg-icons/faGoogleDrive";
 import { faLink } from "@fortawesome/free-solid-svg-icons/faLink";
 import { GDRIVE_TO_DIRECT_QUEUE, gDriveRe } from "../../../config/Constants";
-
-const secondary = ({ gDriveLink, directLink, size, mimeType, timestamp }) => (
-  <SecondaryComponent
-    primaryLink={directLink}
-    primaryText={"Direct Link"}
-    secondaryLink={gDriveLink}
-    secondaryText={"Google Drive Link"}
-    size={size}
-    mimeType={mimeType}
-    timestamp={timestamp}
-    primaryIcon={faLink}
-    secondaryIcon={faGoogleDrive}
-  />
-);
-
-const transferring = ({ message, percentage }) => (
-  <TransferringComponent primaryText={message} percentage={percentage} />
-);
+import CommonBase from "../BluePrints/CommonBase";
 
 const GDriveToDirect = () => {
   return (
-    <TransfersBase
-      dbPath={GDRIVE_TO_DIRECT_QUEUE}
+    <CommonBase
       regExpString={gDriveRe}
+      dbPath={GDRIVE_TO_DIRECT_QUEUE}
       validationErrorMessage={
         "The URL must be a valid Google Drive file/folder export URL"
       }
       submitFN={queueTransfer}
       title={<>Add a Google Drive link to convert to a Direct link</>}
       placeholder={"Google Drive Link"}
-      secondaryComponent={secondary}
-      transferringComponent={transferring}
+      toText={"Direct Link"}
+      toIcon={faLink}
+      toLink={"directLink"}
+      fromText={"Google Drive Link"}
+      fromIcon={faGoogleDrive}
+      fromLink={"gDriveLink"}
     />
   );
 };
