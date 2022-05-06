cb62b8a080bf191e8cc5e4991f1271168adf2f4f
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-05-07 01:44:57 +0530

    refactor torrents to google drive components

--- src/components/Transfers/TorrentsToGDrive/index.js
+++ src/components/Transfers/TorrentsToGDrive/index.js
@@ -1,34 +1,9 @@
 import React from "react";
 import { queueTransfer } from "../../../store/actions";
-import SecondaryComponent from "../TransfersBase/SecondaryComponent";
-import TransferringComponent from "../TransfersBase/TransferringComponent";
 import { faGoogleDrive } from "@fortawesome/free-brands-svg-icons/faGoogleDrive";
 import { magnetRe, TORRENTS_TO_GDRIVE_QUEUE } from "../../../config/Constants";
 import { faMagnet } from "@fortawesome/free-solid-svg-icons/faMagnet";
-import TorrentsComponent from "../TransfersBase/TorrentsComponent";
-import TorrentsBase from "../TransfersBase/TorrentsBase";
-
-const secondary = ({ gDriveLink, magnetLink, size, mimeType, timestamp }) => (
-  <SecondaryComponent
-    primaryLink={gDriveLink}
-    primaryText={"Google Drive Link"}
-    secondaryLink={magnetLink}
-    secondaryText={"Magnet Link"}
-    size={size}
-    mimeType={mimeType}
-    timestamp={timestamp}
-    primaryIcon={faGoogleDrive}
-    secondaryIcon={faMagnet}
-  />
-);
-
-const torrents = (torrentsData) => (
-  <TorrentsComponent torrentsData={torrentsData} />
-);
-
-const transferring = ({ message, percentage }) => (
-  <TransferringComponent primaryText={message} percentage={percentage} />
-);
+import TorrentsBase from "../BluePrints/TorrentsBase";
 
 const TorrentsToGDrive = () => {
   return (
@@ -39,9 +14,12 @@ const TorrentsToGDrive = () => {
       submitFN={queueTransfer}
       title={<>Add a magnet link to convert to a Google Drive link</>}
       placeholder={"Magnet Link"}
-      secondaryComponent={secondary}
-      transferringComponent={transferring}
-      torrentsComponent={torrents}
+      toText={"Google Drive Link"}
+      toIcon={faGoogleDrive}
+      toLink={"gDriveLink"}
+      fromText={"Magnet Link"}
+      fromIcon={faMagnet}
+      fromLink={"magnetLink"}
     />
   );
 };
