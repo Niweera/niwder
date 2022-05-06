e636579225a2b5256b733ec956f78ebc56110340
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-05-07 00:01:35 +0530

    refactor google drive to mega.nz components (update) fix rendering issue in transferred component

--- src/components/Transfers/GDriveToMega/index.js
+++ src/components/Transfers/GDriveToMega/index.js
@@ -1,43 +1,27 @@
 import React from "react";
 import { queueTransfer } from "../../../store/actions";
-import TransfersBase from "../TransfersBase";
-import SecondaryComponent from "../TransfersBase/SecondaryComponent";
-import TransferringComponent from "../TransfersBase/TransferringComponent";
 import { faGoogleDrive } from "@fortawesome/free-brands-svg-icons/faGoogleDrive";
 import { faM } from "@fortawesome/free-solid-svg-icons/faM";
 import { GDRIVE_TO_MEGA_QUEUE, gDriveRe } from "../../../config/Constants";
-
-const secondary = ({ gDriveLink, megaLink, size, mimeType, timestamp }) => (
-  <SecondaryComponent
-    primaryLink={megaLink}
-    primaryText={"Mega.nz Link"}
-    secondaryLink={gDriveLink}
-    secondaryText={"Google Drive Link"}
-    size={size}
-    mimeType={mimeType}
-    timestamp={timestamp}
-    primaryIcon={faM}
-    secondaryIcon={faGoogleDrive}
-  />
-);
-
-const transferring = ({ message, percentage }) => (
-  <TransferringComponent primaryText={message} percentage={percentage} />
-);
+import CommonBase from "../BluePrints/CommonBase";
 
 const GDriveToMega = () => {
   return (
-    <TransfersBase
-      dbPath={GDRIVE_TO_MEGA_QUEUE}
+    <CommonBase
       regExpString={gDriveRe}
+      dbPath={GDRIVE_TO_MEGA_QUEUE}
       validationErrorMessage={
         "The URL must be a valid Google Drive file/folder export URL"
       }
       submitFN={queueTransfer}
       title={<>Add a Google Drive link to convert to a Mega.nz link</>}
       placeholder={"Google Drive Link"}
-      secondaryComponent={secondary}
-      transferringComponent={transferring}
+      toText={"Mega.nz Link"}
+      toIcon={faM}
+      toLink={"megaLink"}
+      fromText={"Google Drive Link"}
+      fromIcon={faGoogleDrive}
+      fromLink={"gDriveLink"}
     />
   );
 };
