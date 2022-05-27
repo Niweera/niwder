9c74d491d91d4d2d0b857fa9b91e1e0ba43a4d83
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-05-27 14:54:18 +0530

    remove createSelectorFactory.js

--- src/utils/createSelectorFactory.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-import { get } from "lodash";
-
-export const createSelectorFactory = (path) =>
-  createSelector(
-    (state) => get(state, path, null),
-    (data) => data
-  );
